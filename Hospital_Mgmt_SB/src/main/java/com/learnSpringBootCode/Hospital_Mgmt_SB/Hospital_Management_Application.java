a914ef69c5c2035b8e0e0572bef814b2f0b1275b
Shekh-Taukir <shekhtaukir13@gmail.com>
2026-03-27 21:20:42 +0800

    ER 04 - Application named changed
    Actual Changes:
    - Changed the name of main application

--- Hospital_Mgmt_SB/src/main/java/com/learnSpringBootCode/Hospital_Mgmt_SB/Hospital_Management_Application.java
+++ Hospital_Mgmt_SB/src/main/java/com/learnSpringBootCode/Hospital_Mgmt_SB/Hospital_Management_Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class Hospital_Management_Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(Hospital_Management_Application.class, args);
 	}
 
 }
